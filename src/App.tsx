fc045bfeb61d3c539d90a602f7a05edd6affb7f2
aro1994 <aro1994125@mail.ru>
2022-03-24 14:24:24 +0400

    remove a class name

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.tsx</code> save to.
         </p>
         <a
-          className="App-link"
+
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
