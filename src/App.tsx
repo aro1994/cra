76727261e59d83ca3d5ed571ea215d655a69758e
aro1994 <aro1994125@mail.ru>
2022-03-26 16:23:32 +0400

    start new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { FC } from 'react';
 import styles from './app.module.scss'
-import { Header } from './components/header/Header';
-import { Technologies } from './components/technologies/Technologies';
+
 
 
 
 export const App: FC = ()=>{
   return <div>
-    <Header/>
-    <Technologies/>
-    
+
   </div>
 
 }
