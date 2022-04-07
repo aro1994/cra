86104aac055088b4dd267feb7dc9ba9ce24eff96
aro1994 <aro1994125@mail.ru>
2022-04-07 15:05:43 +0400

    modified Profile

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react";
+import { Avatar } from "../../components/avatar/Avatar";
+import { Message } from "../../components/message/Message";
+import { Post } from "../../components/post/Post";
 import shoes from "../../img/shoes.jpg"
 import styles from "./profile.module.scss"
 
 
 export const Profile:FC = ()=>{
-	return <div>
+	return <div className={styles.profile}>
 		<img src={shoes} alt="shoes" className={styles.img}/>
+		<Avatar/>
+		<Message/>
+		<Post/>
+		<Post/>
+		<Post/>
+		<Post/>
+
 	</div>
 
 }
