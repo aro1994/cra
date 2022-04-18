e9d3853bad691e49c0f645c2471a99687d98eda2
aro1994 <aro1994125@mail.ru>
2022-04-18 14:20:48 +0400

    add app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 
-import { Header } from './components/header/Header';
-import { Main } from './components/main/main';
-import { Nav } from './components/nav/Nav';
+import { Header } from '../components/header/Header';
+import { Main } from '../components/main/main';
+import { Nav } from '../components/nav/Nav';
 
 import styles from './app.module.scss'
 
